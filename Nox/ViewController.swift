042e5daed823ff870be6b3e31ba179cde603816d
Александр Новиков <theflatladder@gmail.com>
2024-01-16 15:04:07 +0500

    init

---
+++ Nox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nox
+//
+//  Created by Александр Новиков on 16.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
