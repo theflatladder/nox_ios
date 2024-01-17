b0043eceaba5aafca075e579e7b1c8141e1049c1
Александр Новиков <theflatladder@gmail.com>
2024-01-17 16:34:10 +0500

    switch to swift ui

---
+++ Nox/NoxApp.swift
@@ -0,0 +1,32 @@
+//
+//  NoxApp.swift
+//  Nox
+//
+//  Created by Александр Новиков on 17.01.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NoxApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
