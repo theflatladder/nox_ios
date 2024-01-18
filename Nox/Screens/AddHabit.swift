bbc5897a346e892996866706fa41814625e310fa
Александр Новиков <theflatladder@gmail.com>
2024-01-19 00:21:43 +0500

    add habit sheet

--- Nox/Screens/AddHabit.swift
+++ Nox/Screens/AddHabit.swift
@@ -15,15 +15,15 @@ struct AddHabit: View {
         VStack(spacing: 24){
             Text("New habit")
                 .font(.RubikSemiBold(24))
-                .foregroundColor(Color.accentColor)
                 .padding(.top, 16)
             MyTextField(title: "Title", value: $newHabit.title)
             MyStepper(title: "Max count", value: $newHabit.maxCount)
             Spacer()
         }
+        .background(.background)
         .padding(16)
-        .onTapGesture { //need to be on tap of the whole sheet somehow
-            UIApplication.shared.endEditing()
+        .onTapGesture {
+            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
         }
     }
     
@@ -32,10 +32,4 @@ struct AddHabit: View {
 
 #Preview {
     AddHabit()
-}
-
-extension UIApplication {
-    func endEditing() {
-        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-    }
 }
